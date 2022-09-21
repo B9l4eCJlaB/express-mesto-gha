f50ddb74bbb692bc68c520fe3d3904e7f2f51afa
Vyacheslav <90191102+B9l4eCJlaB@users.noreply.github.com>
2022-09-22 00:19:44 +0300

    Delete middlewares directory

--- middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const statusCodes = require('../utils/constants');
-
-module.exports = ((err, req, res, next) => {
-  const { statusCode = statusCodes.DEFAULT, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === statusCodes.DEFAULT ? 'На сервере произошла ошибка' : message,
-  });
-  next();
-});
